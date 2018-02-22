e3de0e59ce60c6a35cd0b613d889560a6394a5d7
Steven Kay <stevendkay@gmail.com>
2018-02-22 09:21:22 +0000

    stub out tests

--- test_arraychunker.py
+++ test_arraychunker.py
@@ -10,6 +10,24 @@ class TestArrayChunker(unittest.TestCase):
         self.assertEqual(get_chunk_size(5, 3), 2)  # expect 3 chunks of 2, [221]
         self.assertEqual(get_chunk_size(6, 1), 6)  # one chunk, whole array
 
+    def test_split_even_size_chunks(self):
+        self.assertEqual(True, False)
+
+    def test_split_chunks_with_remainder(self):
+        self.assertEqual(True, False)
+
+    def test_split_into_no_chunks(self):
+        self.assertEqual(True, False)
+
+    def test_split_into_one_chunk(self):
+        self.assertEqual(True, False)
+
+    def test_spilt_into_too_many_chunks(self):
+        self.assertEqual(True, False)
+
+    def test_split_empty_array(self):
+        self.assertEqual(True, False)
+
 
 if __name__ == '__main__':
     unittest.main()
