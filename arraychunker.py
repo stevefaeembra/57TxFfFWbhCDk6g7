4de1fbd07c7dc0ea014cf8bc86c26c800b6d2bfc
Steven Kay <stevendkay@gmail.com>
2018-02-22 09:09:23 +0000

    initial test case

---
+++ arraychunker.py
@@ -0,0 +1,8 @@
+"""
+Given an array of length >= 0, and a positive integer N,
+return the contents of the array divided into N equally sized arrays.
+Where the size of the original array cannot be divided equally by N,
+the final part should have length equal to the remainder.
+"""
+
+
---
+++ test_arraychunker.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestArrayChunker(unittest.TestCase):
+
+    def test_get_chunk_size(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
